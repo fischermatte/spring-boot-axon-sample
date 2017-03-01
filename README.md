3a5642e12e72dbd7fdeefde0b9bdedf87567f862
fischermatte <fischermatte@posteo.net>
2017-03-01 14:17:39 +0100

    works now with axon 3

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 Introduction
 ============
-This is a sample application to demonstrate Spring Boot, ElasticSearch and Axon Framework.
+This is a sample application to demonstrate Spring Boot, ElasticSearch and Axon Framework V3.
 
 The Todo application makes use of the following design patterns:
 - Domain Driven Design
@@ -21,7 +21,7 @@ Browse to http://localhost:8080/index.html
 Implementation
 ==============
 Implementation notes:
-- The event store is backed by a filesystem implementation which comes with Axon
+- The event store is currently backed by a InMemory implementation which comes with Axon
 - The query model is backed by a local ElasticSearch node (running in the same JVM) using Spring Data ElasticSearch
 - The user interface is updated asynchronously via stompjs over websockets using Spring Websockets support
 
--- src/main/java/nl/avthart/todo/app/configuration/AxonConfiguration.java
+++ src/main/java/nl/avthart/todo/app/configuration/AxonConfiguration.java
@@ -3,18 +3,10 @@ package nl.avthart.todo.app.configuration;
 import nl.avthart.todo.app.domain.task.Task;
 import org.axonframework.commandhandling.AggregateAnnotationCommandHandler;
 import org.axonframework.commandhandling.CommandBus;
-import org.axonframework.commandhandling.SimpleCommandBus;
-import org.axonframework.commandhandling.gateway.CommandGateway;
-import org.axonframework.eventhandling.EventBus;
-import org.axonframework.eventhandling.SimpleEventBus;
 import org.axonframework.eventsourcing.EventSourcingRepository;
-import org.axonframework.eventsourcing.eventstore.EmbeddedEventStore;
+import org.axonframework.eventsourcing.eventstore.EventStorageEngine;
 import org.axonframework.eventsourcing.eventstore.EventStore;
 import org.axonframework.eventsourcing.eventstore.inmemory.InMemoryEventStorageEngine;
-import org.axonframework.messaging.interceptors.BeanValidationInterceptor;
-import org.axonframework.spring.commandhandling.gateway.CommandGatewayFactoryBean;
-import org.axonframework.spring.config.EnableAxon;
-import org.axonframework.spring.config.annotation.AnnotationCommandHandlerBeanPostProcessor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
@@ -22,53 +14,29 @@ import org.springframework.context.annotation.Profile;
 import static nl.avthart.todo.app.configuration.Profiles.LOCAL;
 
 /**
- * Axon Java Configuration with reasonable defaults like SimpleCommandBus, SimpleEventBus and GenericJpaRepository.
+ * Axon Java Configuration with inmemory eventsourcing repository.
  *
  * @author albert
+ * @author fischermatte
  */
-@EnableAxon
 @Configuration
 public class AxonConfiguration {
 
-    @Bean
-    public AnnotationCommandHandlerBeanPostProcessor annotationCommandHandlerBeanPostProcessor() {
-        return new AnnotationCommandHandlerBeanPostProcessor();
-    }
-
-    @Bean
-    public EventBus eventBus() {
-        return new SimpleEventBus();
-    }
-
-    @Bean
-    public CommandBus commandBus() {
-        SimpleCommandBus commandBus = new SimpleCommandBus();
-        commandBus.registerHandlerInterceptor(new BeanValidationInterceptor());
-        return commandBus;
-    }
-
-    @Bean
-    public CommandGatewayFactoryBean<CommandGateway> commandGatewayFactoryBean() {
-        CommandGatewayFactoryBean<CommandGateway> factory = new CommandGatewayFactoryBean<CommandGateway>();
-        factory.setCommandBus(commandBus());
-        return factory;
-    }
-
-    @Bean
-    public EventSourcingRepository<Task> taskRepository() {
-        return new EventSourcingRepository<Task>(Task.class, eventStore());
-    }
-
     @Profile(LOCAL)
     @Bean
-    public EventStore eventStore() {
-        return new EmbeddedEventStore(new InMemoryEventStorageEngine());
+    public EventStorageEngine eventStorageEngine(){
+        return new InMemoryEventStorageEngine();
     }
 
     @Bean
-    public AggregateAnnotationCommandHandler<Task> taskCommandHandler() {
-        AggregateAnnotationCommandHandler<Task> taskCommandHandler = new AggregateAnnotationCommandHandler<Task>(Task.class, taskRepository());
-        taskCommandHandler.subscribe(commandBus());
+    public EventSourcingRepository<Task> taskRepository(EventStore eventStore) {
+        return new EventSourcingRepository<Task>(Task.class, eventStore);
+    }
+
+    @Bean
+    public AggregateAnnotationCommandHandler<Task> taskCommandHandler(CommandBus commandBus, EventSourcingRepository<Task> taskRepository) {
+        AggregateAnnotationCommandHandler<Task> taskCommandHandler = new AggregateAnnotationCommandHandler<Task>(Task.class, taskRepository);
+        taskCommandHandler.subscribe(commandBus);
         return taskCommandHandler;
     }
 }
